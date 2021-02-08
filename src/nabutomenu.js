cfdc84983b6419973c9d7a3bb2cd760ea1ee0cc2
carlso6375 <78713956+carlso6375@users.noreply.github.com>
2021-02-08 01:10:47 -0300

    Add files via upload

---
+++ src/nabutomenu.js
@@ -0,0 +1,12 @@
+const solracmenu = (prefix, pushname) => {
+    return `◪ *Comandos do ninem*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.solracmenu = solracmenu
